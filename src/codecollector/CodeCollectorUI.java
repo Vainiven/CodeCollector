4bafce0c6619880fc210045c4fdf8fe2a8e7181b
Vainiven <96945723+Vainiven@users.noreply.github.com>
2023-07-19 22:35:22 +0200

    Newest added .class files

--- src/codecollector/CodeCollectorUI.java
+++ src/codecollector/CodeCollectorUI.java
@@ -1,16 +1,21 @@
 package codecollector;
 
-import java.awt.FlowLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
-import java.io.BufferedWriter;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -20,44 +25,54 @@ import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
 
 public class CodeCollectorUI extends JFrame {
-
-	/**
-	 * @author Vainiven https://github.com/Vainiven
-	 */
-
-	private static final long serialVersionUID = 1L;
 	private File sourceDirectory;
 	private File outputFile;
 	private JLabel sourceLabel;
 	private JLabel outputLabel;
 	private JProgressBar progressBar;
+	private JLabel filesLabel;
 
 	public CodeCollectorUI() {
+		super("CodeCollector by Vainiven");
+
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		getContentPane().setBackground(Color.DARK_GRAY);
+
 		JButton sourceButton = new JButton("Select Source Directory");
 		sourceButton.addActionListener(this::selectSourceDirectory);
+		sourceButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 		JButton outputButton = new JButton("Select Output File");
 		outputButton.addActionListener(this::selectOutputFile);
+		outputButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 		JButton collectButton = new JButton("Collect");
 		collectButton.addActionListener(this::collectCode);
+		collectButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 		sourceLabel = new JLabel("Source Directory: None");
+		sourceLabel.setForeground(Color.WHITE);
 		outputLabel = new JLabel("Output File: None");
+		outputLabel.setForeground(Color.WHITE);
+		filesLabel = new JLabel("");
+		filesLabel.setForeground(Color.WHITE);
 
 		progressBar = new JProgressBar(0, 100);
+		progressBar.setStringPainted(true);
 		progressBar.setVisible(false);
-
-		setLayout(new FlowLayout());
+		progressBar.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 		add(sourceButton);
 		add(sourceLabel);
+		add(Box.createRigidArea(new Dimension(0, 5)));
 		add(outputButton);
 		add(outputLabel);
+		add(Box.createRigidArea(new Dimension(0, 5)));
 		add(collectButton);
 		add(progressBar);
+		add(filesLabel);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		pack();
+		setSize(500, 200);
 	}
 
 	private void selectSourceDirectory(ActionEvent e) {
@@ -89,28 +104,35 @@ public class CodeCollectorUI extends JFrame {
 		}
 
 		progressBar.setValue(0);
+		progressBar.setString(null);
 		progressBar.setVisible(true);
+		filesLabel.setText("");
 		pack();
 
 		Executors.newSingleThreadExecutor().execute(() -> {
-			try (BufferedWriter writer = Files.newBufferedWriter(outputFile.toPath(), StandardCharsets.UTF_8,
-					StandardOpenOption.CREATE)) {
+			try (OutputStream writer = new BufferedOutputStream(new FileOutputStream(outputFile))) {
+				AtomicLong javaFiles = new AtomicLong();
+				AtomicLong classFiles = new AtomicLong();
+
 				long totalFiles = Files.walk(sourceDirectory.toPath()).filter(Files::isRegularFile)
-						.filter(path -> path.toString().endsWith(".java")).count();
+						.filter(path -> path.toString().endsWith(".java") || path.toString().endsWith(".class"))
+						.peek(path -> {
+							if (path.toString().endsWith(".java")) {
+								javaFiles.incrementAndGet();
+							} else {
+								classFiles.incrementAndGet();
+							}
+						}).count();
 
 				long[] processedFiles = { 0 };
 
 				Files.walk(sourceDirectory.toPath()).filter(Files::isRegularFile)
-						.filter(path -> path.toString().endsWith(".java")).forEach(path -> {
+						.filter(path -> path.toString().endsWith(".java") || path.toString().endsWith(".class"))
+						.forEach(path -> {
 							try {
-								Files.lines(path, StandardCharsets.UTF_8).forEach(line -> {
-									try {
-										writer.write(line);
-										writer.newLine();
-									} catch (IOException ex) {
-										throw new UncheckedIOException(ex);
-									}
-								});
+								byte[] bytes = Files.readAllBytes(path);
+								writer.write(bytes);
+								writer.write(System.lineSeparator().getBytes()); // Add a newline after each file
 								processedFiles[0]++;
 								int progress = (int) (100 * processedFiles[0] / totalFiles);
 								SwingUtilities.invokeLater(() -> progressBar.setValue(progress));
@@ -118,9 +140,14 @@ public class CodeCollectorUI extends JFrame {
 								throw new UncheckedIOException(ex);
 							}
 						});
+
+				SwingUtilities.invokeLater(() -> filesLabel.setText(String
+						.format("Processed %d .java files and %d .class files", javaFiles.get(), classFiles.get())));
 			} catch (IOException ex) {
 				JOptionPane.showMessageDialog(this, "An error occurred: " + ex.getMessage());
 			}
+
+			SwingUtilities.invokeLater(() -> progressBar.setString("DONE"));
 		});
 	}
 
